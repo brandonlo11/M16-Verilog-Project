80750d8508a67aa17eb868ec082715190b111885
brandonlo11 <103808636+brandonlo11@users.noreply.github.com>
2022-05-01 15:10:15 -0700

    Update control.v

--- control.v
+++ control.v
@@ -27,16 +27,16 @@ always @(*) begin
     
     sat_last = 4'b1000;
     
+    if (overflow[3] && saturate)begin
+      sat_enable = 4'b1111;
+    end
+    
     if(overflow[3]) begin
       if(!sign[3]) begin
         sat_sign = 4'b1111;
       end else begin
         sat_sign = 4'b0000;
       end
-    end
-
-    if (overflow[3] && saturate)begin
-      sat_enable = 4'b1111;
     end
     
   end
@@ -48,6 +48,16 @@ always @(*) begin
     carry_in[0] = 1'b0;
     
     sat_last = 4'b1010;
+    
+    if (overflow[3] && saturate) begin
+      sat_enable[3] = 1;
+      sat_enable[2] = 1;
+    end
+    
+    if (overflow[1] && saturate) begin
+      sat_enable[1] = 1;
+      sat_enable[0] = 1;
+    end
     
     if(overflow[3]) begin 
       if(!sign[3]) begin
@@ -67,16 +77,6 @@ always @(*) begin
         sat_sign[1] = 0;
         sat_sign[0] = 0;
       end
-    end
-    
-    if (overflow[3] && saturate) begin
-      sat_enable[3] = 1;
-      sat_enable[2] = 1;
-    end
-    
-    if (overflow[1] && saturate) begin
-      sat_enable[1] = 1;
-      sat_enable[0] = 1;
     end
     
   end
