3c415913bdc4413a2cf39eb9a320e222e3b41933
brandonlo11 <103808636+brandonlo11@users.noreply.github.com>
2022-05-01 01:07:44 -0700

    Update aludraft.v

--- aludraft.v
+++ aludraft.v
@@ -5,44 +5,33 @@ module alu(
     output reg [31:0] c
 );
 
-wire d, e, f, g, h, i, j;
-wire [3:0] carry = 4'b0000;
-  wire [3:0] reverse = 4'b0000;
+  wire [3:0] carry_out;
+  wire [3:0] carry_in;
+  wire [3:0] sign;
+  wire [3:0] overflow;
+  wire [3:0] sat_enable;
+  wire [3:0] sat_sign;
+  wire [3:0] sat_last;
+  wire [31:0] d;
+  wire [31:0] e;
+  wire [3:0]
   
-  add8 add1(.a([7:0]a), .b([7:0]b), .carry_in(reverse[0]), .c(d), .sign(e), .overflow(f), .carry_out(g));
-  carry[0] = g;
-  //control c1(.saturate(saturate), .width(width), .sign(e), .overflow(f), .carry out(carry), .carry in(g), .sat enable(), .sat sign(), .sat last());
+  add8 add1(.a([7:0]a), .b([7:0]b), .carry_in(carry_in[0]), .c([7:0]d), .sign(sign[0]), .overflow(overflow[0]), .carry_out(carry_out[0]));
   
-  add8 add2(.a([15:8]a), .b([15:8]b), .carry_in(reverse[1]), .c(d), .sign(e), .overflow(f), .carry_out(g));
-  carry[1] = g;
- // control c2(.saturate(saturate), .width(width), .sign(e), .overflow(f), .carry out(carry), .carry in(g), .sat enable(), .sat sign(), .sat last());
+  add8 add2(.a([15:8]a), .b([15:8]b), .carry_in(carry_in[1]), .c([15:8]d), .sign(sign[1]), .overflow(overflow[1]), .carry_out(carry_out[1]));
   
-  add8 add3(.a([23:16]a), .b([23:16]b), .carry_in(reverse[2]), .c(d), .sign(e), .overflow(f), .carry_out(g);
-  carry[2] = g;
- // control c3(.saturate(saturate), .width(width), .sign(e), .overflow(f), .carry out(carry), .carry in(g), .sat enable(), .sat sign(), .sat last());
+  add8 add3(.a([23:16]a), .b([23:16]b), .carry_in(carry_in[2]), .c([23:16]d), .sign(sign[2]), .overflow(overflow[2]), .carry_out(carry_out[2]);
             
-            add8 add4(.a([31:24]a), .b([31:24]b), .carry_in(reverse[3]), .c(d), .sign(e), .overflow(f), .carry_out(g));
-  carry[3] = g;
- // control c4(.saturate(saturate), .width(width), .sign(e), .overflow(f), .carry out(carry), .carry in(g), .sat enable(), .sat sign(), .sat last());
+  add8 add4(.a([31:24]a), .b([31:24]b), .carry_in(carry_in[3]), .c([31:24]d), .sign(sign[3]), .overflow(overflow[3]), .carry_out(carry_out[3]));
 
-  control c1(.saturate(saturate), .width(width), .sign(e), .overflow(f), .carry out(carry), .carry in(reverse), .sat enable(), .sat sign(), .sat last());
-
+  control c1(.saturate(saturate), .width(width), .sign(sign), .overflow(overflow), .carry_out(carry_out), .carry_in(carry_in), .sat_enable(sat_enable), .sat_sign(sat_sign), .sat_last(sat_last));
+            
+saturation sat1(.a([7:0]d), .sat_enable(sat_enable[0]), .sat_sign(sat_sign[0]), .sat_last(sat_last[0]), .out([7:0]e));
+            
+saturation sat1(.a([15:8]d), .sat_enable(sat_enable[1]), .sat_sign(sat_sign[1]), .sat_last(sat_last[1]), .out([15:8]e));
+            
+saturation sat1(.a([23:16]d), .sat_enable(sat_enable[2]), .sat_sign(sat_sign[2]), .sat_last(sat_last[2]), .out([23:16]e));
   
-  
+saturation sat1(.a([31:24]d), .sat_enable(sat_enable[3]), .sat_sign(sat_sign[3]), .sat_last(sat_last[3]), .out([31:24]e));
+            
 endmodule
-
-
-
-module alu(
-  input [31:0] a, b,
-  input saturate,
-  input [1:0] width,
-  output reg [31:0] c
-);
-  
-  saturation sat1(.a(), .sat_enable(), .sat_sign(), .sat_last(), .out());
-  add8 add1(.a(), .b(), .carry_in(), .c(), .sign(), .overflow(), .carry_out());
-  
-  
-always @(*) begin
-  
